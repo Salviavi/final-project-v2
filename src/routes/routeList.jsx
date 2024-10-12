34c0a44fee8f072bcc5c040dbfd6cbbf9fead66d
Salviavi <entp7w8@gmail.com>
2024-10-12 12:03:55 +0700

    adding routes to the pages

---
+++ src/routes/routeList.jsx
@@ -0,0 +1,38 @@
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import ProtectedRoute from "./ProtectedRoute";
+import ProtectedAfterLogin from "./ProtectedAfterLogin";
+import Register from "../pages/Register";
+import Landing from "../pages/Landing";
+import Dashboard from "../pages/Landing";
+
+
+export const routeList = [
+    {
+        path: "/",
+        element: <Landing />
+
+    },
+    {
+        path: "/dashboard",
+        element: (
+        <ProtectedRoute>
+            <Dashboard />
+        </ProtectedRoute>
+        ),
+
+    },
+    {
+        path: "/register",
+        element: <Register />
+    },
+    {
+        path: "/login",
+        element: (
+            <ProtectedAfterLogin>
+                <Login />
+            </ProtectedAfterLogin>
+        ),
+    },
+
+];
